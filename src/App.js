c1c3767d0a7fad54d7a7a782e7d7c774a85dd569
alimady <alismade1997@gmail.com>
2023-12-23 07:02:54 -0800

    i am ali mady king and de lllt

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       <h1>hi there i am react developer</h1>
+       <h1>hi there i am react and laravell developer</h1>
     </div>
   );
 }
